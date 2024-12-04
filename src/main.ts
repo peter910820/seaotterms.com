bc6aaa051c32c60bb42ca536c4ad4dc004181982
peter910820 <peter910820@gmail.com>
2024-12-05 00:09:25 +0800

    set global css

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
-
-// import store from "./store/store";
+// global stylesheets
+import "./assets/styles/button.css";
 
 createApp(App).use(router).use(store).mount("#app");

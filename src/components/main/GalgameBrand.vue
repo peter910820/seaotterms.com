d9b7e8fca92286a51a76a414e899b122edf7bdfb
peter910820 <peter910820@gmail.com>
2025-02-22 22:31:32 +0800

    update GalgameBrand.vue spec

--- src/components/main/GalgameBrand.vue
+++ src/components/main/GalgameBrand.vue
@@ -1,52 +1,46 @@
 <template>
-  <div class="row wow animate__flipInX">
-    <h1>Galgameブランド紀錄</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>ブランド</th>
-          <th>攻略數</th>
-          <th>總遊戲數</th>
-          <th>狀態</th>
-          <th>解散</th>
-          <th>修改</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="galgameBrand in galgameBrandData" :key="galgameBrand.Brand">
-          <td>
-            <b>{{ galgameBrand.brand }}</b>
-          </td>
-          <td>
-            <b>{{ galgameBrand.completed }}</b>
-          </td>
-          <td>
-            <b>{{ galgameBrand.total }}</b>
-          </td>
-
-          <td v-if="galgameBrand.annotation === '制霸'">
-            <b>
-              <font color="blue">{{ galgameBrand.annotation }}</font>
-            </b>
-          </td>
-          <td v-else>{{ galgameBrand.annotation }}</td>
-
-          <td>
-            <font color="red">{{ galgameBrand.dissolution ? "解散" : "" }}</font>
-          </td>
-          <td>
-            <button class="button-simple">
-              <router-link :to="`/galgamebrand/update/${galgameBrand.brand}`">
-                我API還沒寫QQ
-              </router-link>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <button class="button-simple">
-      <router-link to="/galgamebrand/insert">新增</router-link>
-    </button>
+  <div class="mainBlock">
+    <div class="col s12 center">
+      <h1>Galgameブランド紀錄</h1>
+    </div>
+    <div class="col s12 galgameBrandInsert">
+      <button class="button-simple">
+        <router-link to="/galgamebrand/insert">點我新增</router-link>
+      </button>
+    </div>
+    <div class="col s12 galgameBrandHeader">
+      <div class="col s3">ブランド</div>
+      <div class="col s1">攻略數</div>
+      <div class="col s2">總遊戲數</div>
+      <div class="col s2">狀態</div>
+      <div class="col s2">解散</div>
+      <div class="col s2">修改</div>
+    </div>
+    <div
+      class="col s12 galgameBrand floatup-div wow animate__bounceIn"
+      v-for="galgameBrand in galgameBrandData"
+      :key="galgameBrand.brand"
+    >
+      <div class="col s3 brand">{{ galgameBrand.brand }}</div>
+      <div class="col s1">{{ galgameBrand.completed }}</div>
+      <div class="col s2">{{ galgameBrand.total }}</div>
+      <div class="col s2" v-if="galgameBrand.annotation === '制霸'">
+        <b>
+          <font color="blue">{{ galgameBrand.annotation }}</font>
+        </b>
+      </div>
+      <div class="col s2" v-else>{{ galgameBrand.annotation }}</div>
+      <div class="col s2">
+        <font color="red">{{ galgameBrand.dissolution ? "解散" : "" }}</font>
+      </div>
+      <div class="col s2 right">
+        <button class="button-simple">
+          <router-link :to="`/galgamebrand/update/${galgameBrand.brand}`">
+            我修改API還沒寫QQ
+          </router-link>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,9 +61,43 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.row {
-  height: 800px;
-  padding: 50px;
+.col {
+  max-height: 150px;
+  height: 150px;
+  padding-top: 20px;
+  margin-top: 10px;
+}
+.mainBlock {
+  padding: 25px;
+}
+.galgameBrandHeader {
+  text-align: center;
+  font-size: x-large;
+  font-weight: bold;
+  max-height: 100%;
+}
+.galgameBrandInsert {
+  text-align: center;
+  max-height: 50px;
+  height: 50px;
+}
+.galgameBrand {
+  text-align: center;
+  max-height: 100%;
+  border: 2px solid rgba(255, 255, 255, 0.3);
+  border-radius: 20px;
+  > div {
+    font-size: x-large;
+    margin-left: 5px;
+    padding: 0px;
+    max-height: 100%;
+  }
+  > .right {
+    text-align: right;
+  }
+}
+.brand {
+  font-weight: bold;
 }
 td {
   font-size: large;

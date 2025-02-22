c41cbe92d91a5cecd3ccde5dc1896b80ef67e12d
peter910820 <peter910820@gmail.com>
2025-02-22 20:28:45 +0800

    add button-simple class

--- src/components/main/GalgameBrand.vue
+++ src/components/main/GalgameBrand.vue
@@ -9,19 +9,44 @@
           <th>總遊戲數</th>
           <th>狀態</th>
           <th>解散</th>
+          <th>修改</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="galgameBrand in galgameBrandData" :key="galgameBrand.Brand">
-          <td>{{ galgameBrand.brand }}</td>
-          <td>{{ galgameBrand.completed }}</td>
-          <td>{{ galgameBrand.total }}</td>
-          <td>{{ galgameBrand.annotation }}</td>
-          <td>{{ galgameBrand.dissolution ? "解散" : "" }}</td>
+          <td>
+            <b>{{ galgameBrand.brand }}</b>
+          </td>
+          <td>
+            <b>{{ galgameBrand.completed }}</b>
+          </td>
+          <td>
+            <b>{{ galgameBrand.total }}</b>
+          </td>
+
+          <td v-if="galgameBrand.annotation === '制霸'">
+            <b>
+              <font color="blue">{{ galgameBrand.annotation }}</font>
+            </b>
+          </td>
+          <td v-else>{{ galgameBrand.annotation }}</td>
+
+          <td>
+            <font color="red">{{ galgameBrand.dissolution ? "解散" : "" }}</font>
+          </td>
+          <td>
+            <button class="button-simple">
+              <router-link :to="`/galgamebrand/update/${galgameBrand.brand}`">
+                我API還沒寫QQ
+              </router-link>
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
-    <button><router-link to="/galgamebrand/insert">新增</router-link></button>
+    <button class="button-simple">
+      <router-link to="/galgamebrand/insert">新增</router-link>
+    </button>
   </div>
 </template>
 
@@ -45,5 +70,11 @@ export default defineComponent({
 .row {
   height: 800px;
   padding: 50px;
+}
+td {
+  font-size: large;
+}
+a {
+  color: black !important;
 }
 </style>

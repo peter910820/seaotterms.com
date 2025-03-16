0c3dcea137a9cc0880fba0247bb3f9864ef33a46
peter910820 <peter910820@gmail.com>
2025-03-16 19:23:06 +0800

    add packaging obfuscation

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require("@vue/cli-service");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: (config) => {
+    config.optimization = {
+      minimize: true, // 啟用程式碼壓縮(此功能有問題)
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: false, // 移除 console.log(不移除)
+              drop_debugger: true, // 移除 debugger
+            },
+            output: {
+              comments: false, // 移除註解
+            },
+            mangle: true, // 混淆變數名稱
+          },
+        }),
+      ],
+    };
+  },
 });

679b359079e703908234b43adaaeb1f9b79b326e
peter910820 <peter910820@gmail.com>
2025-03-25 22:02:22 +0800

    feat: update auth route

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,8 +11,10 @@ import mainRoutes from "./mainRoutes";
 
 // the page need to check the login
 const privatePages = ["/create", "/user-maintain", "/todolist", "/todo-topic"];
-// the page need to check the user
+// the page need to check the blog owner
 const privatePages2 = ["/galgamebrand/insert", "/galgame/insert"];
+const privateRegex = /^\/galgamebrand\/edit\/[^/]+\/?$/;
+const privateRegex2 = /^\/galgame\/edit\/[^/]+\/?$/;
 
 // routes
 const routes: Array<RouteRecordRaw> = [...mainRoutes];
@@ -35,17 +37,16 @@ router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormali
         store.commit("setUserData", {});
         console.log(`${error.response?.status}: ${error.response?.data.msg}`);
         sessionStorage.setItem("msg", `${error.response?.status}: ${error.response?.data.msg}`);
-        alert("使用者沒有權限");
+        alert("使用者尚未登入, 請前往登入");
         next("/login");
       } else {
         console.error(error);
         next("/message");
       }
     }
-  } else if (privatePages2.includes(to.path)) {
+  } else if (privatePages2.includes(to.path) || privateRegex.test(to.path) || privateRegex2.test(to.path)) {
     try {
-      const response = await axios.post("/api/authentication");
-      store.commit("setUserData", response?.data.userData);
+      await axios.post("/api/auth");
       // login now
       next();
     } catch (error) {
@@ -53,8 +54,8 @@ router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormali
         store.commit("setUserData", {});
         console.log(`${error.response?.status}: ${error.response?.data.msg}`);
         sessionStorage.setItem("msg", `${error.response?.status}: ${error.response?.data.msg}`);
-        alert("使用者尚未登入, 請前往登入");
-        next("/login");
+        alert("使用者沒有權限");
+        next("/galgamebrand");
       } else {
         console.error(error);
         next("/message");

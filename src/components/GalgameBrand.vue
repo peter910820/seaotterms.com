539059faa8047ed3fdcb19bd640e6aa1075756f1
peter910820 <peter910820@gmail.com>
2025-03-29 03:09:19 +0800

    style: modify GalgameBrand.vue spec

--- src/components/GalgameBrand.vue
+++ src/components/GalgameBrand.vue
@@ -56,14 +56,14 @@
       <div class="modal-content" @click.stop>
         <h4>{{ selectedBrand }} 通關遊戲資料</h4>
         <div class="col s12 galgameBrandHeader">
-          <div class="col s6">ゲーム</div>
+          <div class="col s6 left-align">ゲーム</div>
           <div class="col s2">発売日</div>
           <div class="col s2">遊玩結束時間</div>
           <div class="col s1">全年齡</div>
           <div class="col s1">修改</div>
         </div>
         <div class="col s12 galgameBrand floatup-div" v-for="brandGames in selectedBrandGames" :key="brandGames.name">
-          <div class="col s6 brand">{{ brandGames.name }}</div>
+          <div class="col s6 left-align">{{ brandGames.name }}</div>
           <div class="col s2">{{ formatDate(brandGames.releaseDate) }}</div>
           <div class="col s2">{{ formatDate(brandGames.endDate) }}</div>
           <div class="col s1" v-if="brandGames.allAges === false">
@@ -166,7 +166,7 @@ export default defineComponent({
   }
 }
 .galgameBrandHeader {
-  text-align: right;
+  text-align: center;
   font-size: x-large;
   font-weight: bold;
   max-height: 80px;

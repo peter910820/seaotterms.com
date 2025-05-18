b8bea83fac6fc17133aad5d5bd7e03fc4aa76c2c
peter910820 <peter910820@gmail.com>
2025-05-18 22:03:14 +0800

    chore: update prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,16 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": ["error", { printWidth: 120, endOfLine: "auto", htmlWhitespaceSensitivity: "ignore" }],
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 120,
+        endOfLine: "auto",
+        htmlWhitespaceSensitivity: "ignore",
+        arrowParens: "always",
+        bracketSpacing: true,
+        jsxSingleQuote: false,
+      },
+    ],
   },
 };

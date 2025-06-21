7e5fa24276e9e822542dc58d9c5bab2a106f94d9
peter910820 <peter910820@gmail.com>
2025-06-22 02:14:42 +0800

    fix: fix system todo page error

--- src/utils/apiHandler.ts
+++ src/utils/apiHandler.ts
@@ -71,7 +71,7 @@ export const getTagInformation = async (mode: number, tagName?: string): Promise
 };
 
 export const getSystemTodo = async (id?: string): Promise<AxiosResponse | null> => {
-  const apiUrl = process.env.VUE_APP_API_URL + id ? `api/system-todos?id=${id}` : "api/system-todos";
+  const apiUrl = process.env.VUE_APP_API_URL + (id ? `api/system-todos?id=${id}` : "api/system-todos");
   try {
     const response = await axios.get(apiUrl);
     return response;
